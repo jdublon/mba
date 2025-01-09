f1da2647e8590bf53cedbe537f001dfedd189701
jdublon <j.dublon@stem.org.uk>
2025-01-09 10:06:06 +0000

    Add black background to text box

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -15,21 +15,24 @@ export const Hero: FC = () => {
           md:bg-[url('/images/hero-image-md.jpg')] 
           lg:bg-[url('/images/hero-image-lg.jpg')]"
       />
-      <div className="absolute bottom-0 left-0 right-0 h-[8.75rem] bg-black sm:h-0 sm:bg-transparent px-4 pt-3">
-        <p className="text-white leading-[1.375rem] tracking-[-0.02rem] font-serif">
-          Hike, bike and snorkel this unique archipelago and encounter its
-          special wildlife, on an adventure immersed in the natural world
-        </p>
-      </div>
 
-      <div className="absolute bottom-[9.5rem] sm:top-auto left-0 right-0 mx-4 inline-flex flex-col justify-start items-start gap-6 sm:gap=[1.62rem]">
-        <h1 className="text-white text-[2rem] sm:text-[3rem] font-extrabold font-sans leading-9 sm:leading-[3.375rem] tracking-[-0.04rem]">
-          ISLAND HOPPING ADVENTURE IN THE GALAPAGOS
-        </h1>
-        <div className="flex gap-6 flex-wrap">
-          <IconLabel icon="location" label="Galapagos islands, Ecuador" />
-          <IconLabel icon="duration" label="9 nights" />
-          <IconLabel icon="difficulty" label="Easy" />
+      <div className="absolute bottom-0 left-0 right-0 inline-flex flex-col justify-start items-start">
+        <div className="mx-4 mb-3 sm:mb-[1.62rem]">
+          <h1 className="text-white text-[2rem] sm:text-[3rem] font-extrabold font-sans leading-9 sm:leading-[3.375rem] tracking-[-0.04rem] mb-6 sm:mb-[1.62rem]">
+            ISLAND HOPPING ADVENTURE IN THE GALAPAGOS
+          </h1>
+          <div className="flex gap-6 flex-wrap">
+            <IconLabel icon="location" label="Galapagos islands, Ecuador" />
+            <IconLabel icon="duration" label="9 nights" />
+            <IconLabel icon="difficulty" label="Easy" />
+          </div>
+        </div>
+
+        <div className="bg-black sm:bg-transparent w-screen px-4 pt-3 sm:pt-0">
+          <p className="text-white font-serif text-base leading-[1.375rem] tracking-[-0.02rem] pb-10 sm:pb-12">
+            Hike, bike and snorkel this unique archipelago and encounter its
+            special wildlife, on an adventure immersed in the natural world
+          </p>
         </div>
       </div>
     </div>
