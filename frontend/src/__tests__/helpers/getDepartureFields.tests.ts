585a2e3702a1eeffcdcb1812f5286e5eea5e16d0
jdublon <j.dublon@stem.org.uk>
2025-01-10 12:57:17 +0000

    Update tests

--- frontend/src/__tests__/helpers/getDepartureFields.tests.ts
+++ frontend/src/__tests__/helpers/getDepartureFields.tests.ts
@@ -1,11 +1,14 @@
-import { getDepartureDates } from "@/helpers/getDepartureFields";
+import { getDepartureFields } from "@/helpers";
+import { mockAllDepartures } from "../fixtures";
 
-describe("Helper: getDepartureDates", () => {
+describe("Helper: getDepartureFields", () => {
   it("SHOULD return correctly formatted start and end date WHEN start date and duration are passed", async () => {
-    const res = getDepartureDates("2025-11-13", 9);
-    expect(res).toEqual({
-      startDate: "13th Nov 2025",
-      endDate: "22nd Nov 2025",
-    });
+    const res = getDepartureFields(mockAllDepartures.results[0], 9);
+    expect(res).toEqual([
+      { label: "Start Date:", value: "19th May 2025" },
+      { label: "End Date:", value: "28th May 2025" },
+      { label: "Price:", value: "£3918" },
+      { label: "Availability:", value: "Spaces available" },
+    ]);
   });
 });
