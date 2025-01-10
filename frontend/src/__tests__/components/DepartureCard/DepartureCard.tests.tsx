7f6a558b70ab530dfdb0e619000582f5dabf0745
jdublon <j.dublon@stem.org.uk>
2025-01-10 13:02:44 +0000

    Update tests on DepartureCard

--- frontend/src/__tests__/components/DepartureCard/DepartureCard.tests.tsx
+++ frontend/src/__tests__/components/DepartureCard/DepartureCard.tests.tsx
@@ -6,37 +6,38 @@ import { mockAllDepartures } from "../../fixtures";
 describe("Component: DepartureCard", () => {
   it("SHOULD match snapshot WHEN rendered", () => {
     const component = render(
-      <DepartureCard
-        departure={mockAllDepartures.results[1]}
-        label="departure-1"
-      />
+      <DepartureCard departure={mockAllDepartures.results[1]} duration={9} />
     );
     expect(component).toMatchSnapshot();
   });
 
   it("SHOULD render departure info as expected WHEN spaces are available", () => {
     const { getByText } = render(
-      <DepartureCard
-        departure={mockAllDepartures.results[1]}
-        label="departure-1"
-      />
+      <DepartureCard departure={mockAllDepartures.results[1]} duration={9} />
     );
 
-    expect(getByText(`Start Date: ${mockAllDepartures.results[1].start_date}`));
-    expect(getByText(`Price: £${mockAllDepartures.results[1].price}`));
-    expect(getByText("Availability: Spaces available"));
+    expect(getByText("Start Date:"));
+    expect(getByText("10th Aug 2025"));
+    expect(getByText("End Date:"));
+    expect(getByText("19th Aug 2025"));
+    expect(getByText("Price:"));
+    expect(getByText("£1715"));
+    expect(getByText("Availability:"));
+    expect(getByText("Spaces available"));
   });
 
   it("SHOULD render departure info as expected WHEN no spaces are available", () => {
     const { getByText } = render(
-      <DepartureCard
-        departure={mockAllDepartures.results[3]}
-        label="departure-3"
-      />
+      <DepartureCard departure={mockAllDepartures.results[3]} duration={3} />
     );
 
-    expect(getByText(`Start Date: ${mockAllDepartures.results[3].start_date}`));
-    expect(getByText(`Price: £${mockAllDepartures.results[3].price}`));
-    expect(getByText("Availability: Sorry this trip is fully booked!"));
+    expect(getByText("Start Date:"));
+    expect(getByText("30th Nov 2025"));
+    expect(getByText("End Date:"));
+    expect(getByText("3rd Dec 2025"));
+    expect(getByText("Price:"));
+    expect(getByText("£2900"));
+    expect(getByText("Availability:"));
+    expect(getByText("Sorry this trip is fully booked!"));
   });
 });
