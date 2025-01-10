5ea665353ac909bf12178b8410887db45debd02d
jdublon <j.dublon@stem.org.uk>
2025-01-10 13:05:24 +0000

    Update test name

--- frontend/src/__tests__/helpers/getDepartureFields.tests.ts
+++ frontend/src/__tests__/helpers/getDepartureFields.tests.ts
@@ -2,7 +2,7 @@ import { getDepartureFields } from "@/helpers";
 import { mockAllDepartures } from "../fixtures";
 
 describe("Helper: getDepartureFields", () => {
-  it("SHOULD return correctly formatted start and end date WHEN start date and duration are passed", async () => {
+  it("SHOULD return correctly formatted departure fields WHEN data is provided", async () => {
     const res = getDepartureFields(mockAllDepartures.results[0], 9);
     expect(res).toEqual([
       { label: "Start Date:", value: "19th May 2025" },
